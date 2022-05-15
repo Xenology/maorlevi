16bdc24a7981696b112ad3cc15a910cf0ef3cc42
Adam Stark <adamstark@Adams-MacBook-Pro-3.local>
2022-05-14 21:09:08 -0600

    Mobile styles

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <link rel="stylesheet" href="/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
@@ -32,9 +32,27 @@
             <iframe width="100%" src="https://www.youtube.com/embed/RgXYp_QvWB0" frameborder="0"></iframe>
           </div>
           <div class="clearfix"></div>
+
+          <div class="mobile-socials">
+            <div class="social-container">
+              <div class="row">
+                <a href="http://bit.do/MaorLeviFacebook" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="http://bit.do/MaorLeviTwitter" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="http://bit.do/MaorLeviInstagram" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="http://bit.do/MaorLeviSnapchat" target="_blank"><i class="fab fa-snapchat"></i></a>
+              </div>
+              <div class="row">
+                <a href="http://bit.do/MLSpotify" target="_blank"><i class="fab fa-spotify"></i></a>
+                <a href="http://bit.do/MaorLeviApple" target="_blank"><i class="fab fa-itunes-note"></i></a>
+                <a href="http://bit.do/MaorLeviSoundcloud" target="_blank"><i class="fab fa-soundcloud"></i></a>
+                <a href="http://bit.do/MaorLeviYoutube" target="_blank"><i class="fab fa-youtube"></i></a>
+                <a href="mailto:info@maorlevi.com" target="_blank"><i class="fas fa-envelope"></i></a>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div class="socials">
+        <div class="desktop-socials">
           <div class="social-container">
             <a href="http://bit.do/MaorLeviFacebook" target="_blank"><i class="fab fa-facebook"></i></a>
             <a href="http://bit.do/MaorLeviTwitter" target="_blank"><i class="fab fa-twitter"></i></a>
@@ -54,9 +72,9 @@
         window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
       </script>
 
-      <script src="/js/vendor/bootstrap.min.js"></script>
+      <script src="js/vendor/bootstrap.min.js"></script>
       <script src="https://kit.fontawesome.com/dbc048e1bc.js" crossorigin="anonymous"></script>
-      <script src="/js/plugins.js"></script>
-      <script src="/js/main.js"></script>
+      <script src="js/plugins.js"></script>
+      <script src="js/main.js"></script>
     </body>
 </html>
