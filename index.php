204c82f426ff66380f5f7a807e8a0f67fd4624a3
Adam Stark <adam.r.stark@gmail.com>
2023-03-18 14:35:39 -0600

    Custom error router

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
+use App\Error\Renderer\HtmlErrorRenderer;
 
+
+require('error.php');
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
@@ -25,4 +29,16 @@ $app->get('/tour', function ($request, $response, $args) {
     return $view->render($response, 'tour.phtml');
 })->setName('tour');
 
-$app->run();
+$app->get('*', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'home.phtml');
+})->setName('all');
+
+
+
+$displayErrorDetails = (bool)($_ENV['DEBUG'] ?? false);
+$errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, true, true);
+$errorHandler = $errorMiddleware->getDefaultErrorHandler();
+$errorHandler->registerErrorRenderer('text/html', HtmlErrorRenderer::class);
+
+  $app->run();
